1c5fdae7f06db4dbc8157f62c4c94a79e9de90c4
Artem Porohov <mac@MacBook-Pro-Apple-2.local>
2023-08-22 00:47:19 +1000

    feat: loading indicator widget

---
+++ reddit_feed/lib/widgets/components/loading_indicator.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+/// Виджет индикатора загрузки
+class LoadingIndicator extends CircularProgressIndicator {
+  const LoadingIndicator({super.key, super.strokeWidth = 2});
+}
